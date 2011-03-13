46a3d9cffb6064ed1042b9d3fe3761dfba8ea8d3
Elijah Insua <tmpvar@gmail.com>
2011-03-13 01:56:28 -0500

    initial commit

---
+++ formation.js
@@ -0,0 +1,11 @@
+(function(window, $) {
+  $.fn.formation = function() {
+    var self = $(this);
+    return self.each(function() {
+      
+      
+      
+      
+    });
+  }
+}(window, window.jQuery));
